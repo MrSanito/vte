23caa483a519e757121b668f87de182b2804e1b2
unknown <1988krishnani@gmail.com>
2026-04-11 14:52:29 +0530

    feat: add Google Tag Manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -97,12 +97,33 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJX8FRDX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <ThemeProvider>
           <Navbar />
           {children}
           <Footer />
           <WhatsAppButton />
         </ThemeProvider>
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NJX8FRDX');
+          `}
+        </Script>
 
         {/* Google Analytics */}
         <Script
