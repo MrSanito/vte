6e753472f052e570fc30f97ceb46312c84f80aa9
unknown <vishalni2004@gmail.com>
2026-06-26 19:17:58 +0530

    added

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
   display: 'swap',
 });
-
+   
 export const metadata = {
   metadataBase: new URL('https://www.vishaltoolsententerprise.in'),
   title: {
